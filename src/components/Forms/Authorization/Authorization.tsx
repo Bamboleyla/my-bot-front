52f3a1824cb4c8f9f786c8de3c4f727d4c8e0812
Dmitriy Vorobjev <dvorobjevredstar@mail.ru>
2022-11-08 18:06:35 +0500

    изменил поле password на странице auth

--- src/components/Forms/Authorization/Authorization.tsx
+++ src/components/Forms/Authorization/Authorization.tsx
@@ -1,24 +1,86 @@
+import * as React from "react";
 import styles from "./authorization.module.scss";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
+import FilledInput from "@mui/material/FilledInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Logo from "./bots.jpg";
+
+interface State {
+  login: string;
+  password: string;
+  showPassword: boolean;
+}
 
 export const Authorization = () => {
+  const [values, setValues] = React.useState<State>({
+    login: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={styles.form}>
-      <Stack spacing={2}>
-        <TextField
-          id="filled-basic"
-          label="Login"
-          variant="filled"
-          size="small"
-        />
-        <TextField
-          id="filled-basic"
-          label="Password"
-          variant="filled"
-          size="small"
-        />
-      </Stack>
+      <img src={Logo} alt="logo" />
+      <div className={styles.wrap}>
+        <Stack spacing={2}>
+          <TextField
+            id="filled-basic"
+            label="Login"
+            variant="filled"
+            size="small"
+            onChange={handleChange("login")}
+            value={values.login}
+          />
+          <FormControl variant="filled">
+            <InputLabel htmlFor="filled-adornment-password">
+              Password
+            </InputLabel>
+            <FilledInput
+              id="filled-adornment-password"
+              type={values.showPassword ? "text" : "password"}
+              size="small"
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Stack>
+      </div>
     </div>
   );
 };

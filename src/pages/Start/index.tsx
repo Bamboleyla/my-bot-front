81040a97c38ab40cc8c69272982c445e192b2b3d
Dmitriy Vorobjev <dvorobjevredstar@mail.ru>
2023-08-30 06:58:54 +0500

    переместил подпись на странице Start

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -4,7 +4,7 @@ import logo from "../../shared/assets/Logo320.svg";
 export const Start = () => {
   return (
     <div className={styles.start} data-testid={"Start"}>
-      <div className={styles.body}>
+      <div className={styles.label}>
         <div className={styles.title}>
           <img src={logo} alt="Logo" />
           <p>Ядро</p>
